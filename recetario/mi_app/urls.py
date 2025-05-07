92badd60772a75208cc611e72e1fbca0f9ccf16f
frank-froz <franklin.huaytalla@tecsup.edu.pe>
2025-05-07 15:50:28 -0500

    creacion del proyecto y la app para la api

---
+++ recetario/mi_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import RecetaViewSet, IngredienteViewSet, CategoriaViewSet
+
+router = DefaultRouter()
+router.register(r'recetas', RecetaViewSet)
+router.register(r'ingredientes', IngredienteViewSet)
+router.register(r'categorias', CategoriaViewSet)
+
+urlpatterns = [
+    path('api/', include(router.urls)),
+]
